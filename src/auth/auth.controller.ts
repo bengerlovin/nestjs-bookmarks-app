f86db47c67dd1afbe304485afd4ada629c107cb0
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-03-18 10:35:41 -0400

    added error handling for prisma, updated model to include relationships between user and bookmarks

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @Post("signup")
-    signup(@Body() dto: AuthDto) {
-        return `${dto.email}, ${dto.password}`
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
-    @Post("signin")
-    signin() {
-        return this.authService.login();
-    }
-
+  @Post('signin')
+  signin() {
+    return this.authService.login();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from "@nestjs/common";
-import { User, Bookmark } from '@prisma/client'
-import { PrismaService } from "src/prisma/prisma.service";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
 
-    constructor(private prismaService: PrismaService) { }
+  login() {
+    return 'I am login from service!';
+  }
 
-    login() {
-        return "I am login from service!"
+  async signup(dto: AuthDto) {
+    // hash password
+    const hash = await argon.hash(dto.password);
+
+    // save new user
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      // remove hash from return object
+      delete user.hash;
+
+      // return new user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
     }
-
-    signup() {
-        return { msg: "hello from signup in service" }
-    }
+  }
 }
