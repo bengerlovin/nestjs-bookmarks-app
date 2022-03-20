6d9d385b3ee657725341a214cfe746e88d6a0682
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-03-20 19:45:16 -0400

    added testing block for signin() function

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -27,6 +27,28 @@ describe('Auth Controller', () => {
       let accessToken: TokenResponse;
 
       beforeEach(async () => {
+        accessToken = await authService.signup(AuthStub());
+      });
+
+      test('call AuthService', () => {
+        expect(authService.signup).toHaveBeenCalled();
+      });
+
+      test('called AuthService with an email and password', () => {
+        expect(authService.signup).toHaveBeenCalledWith(AuthStub());
+      });
+
+      test('return an accesstoken object', () => {
+        expect(accessToken).toEqual(AuthTokenStub());
+      });
+    });
+  });
+
+  describe('Sign in a user', () => {
+    describe('When signin() is invoked it should:', () => {
+      let accessToken: TokenResponse;
+
+      beforeEach(async () => {
         accessToken = await authService.signin(AuthStub());
       });
 
@@ -34,7 +56,11 @@ describe('Auth Controller', () => {
         expect(authService.signin).toHaveBeenCalled();
       });
 
-      test('return an accesstoken', () => {
+      test('called AuthService with an email and password', () => {
+        expect(authService.signin).toHaveBeenCalledWith(AuthStub());
+      });
+
+      test('return an accesstoken object', () => {
         expect(accessToken).toEqual(AuthTokenStub());
       });
     });
